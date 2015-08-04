9f078eb584c55a0ef639cdd6e6ad714dc0881c05
Lucas Meo-Henry <lrm224@cornell.edu>
2015-08-04 11:15:34 -0400

    configured new relic

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=8434800ba1cb1d4e9d1d9a4183e90e29e651199d
 newrelic-install install
